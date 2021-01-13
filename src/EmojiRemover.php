a96ac99554c8eca86d2f8bbf5e672fdd46cd4751
Aris <aris@dxtr.asia>
2021-01-13 12:37:41 +0700

    fix: if text is null or empty string

--- src/EmojiRemover.php
+++ src/EmojiRemover.php
@@ -9,12 +9,16 @@ class EmojiRemover
     /**
      * Filter emoji from text
      *
-     * @param string $text
+     * @param string|null $text
      * @param string $replaceTo
-     * @return string
+     * @return string|null
      */
-    public static function filter(string $text, string $replaceTo = ''): string
+    public static function filter(?string $text, string $replaceTo = ''): ?string
     {
+        if (is_null($text) || $text == '') {
+            return $text;
+        }
+
         try {
             return preg_replace(EmojiRegex::get(), $replaceTo, $text);
         } catch (\Exception $e) {
